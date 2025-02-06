20762513714ee2c71a11f00415c4d1a52cc71f00
Kushagra Singh <iamkushagra2021@gmail.com>
2025-02-06 16:40:40 +0530

    made video and user model

--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT||8000,()=>{
+            console.log(`Server is running on port ${process.env.PORT||8000}`);
+        })
+        
+    })
+    .catch((err) => {
+        console.log("MONGO connection fail",err);
+    });
