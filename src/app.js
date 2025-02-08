10d44901a08683c635fe47f535a745d62e8f71e0
Kushagra Singh <iamkushagra2021@gmail.com>
2025-02-09 01:28:55 +0530

    configured

--- src/app.js
+++ src/app.js
@@ -3,17 +3,22 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 
+
 const app = express();
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
-app.use(cookieParser());
 
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
-export {app}
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users", userRouter);
+
+export {app};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-// require("dotenv").config({path:'./env'});
 import dotenv from "dotenv";
+import express from "express";
+
 dotenv.config();
 
 import connectDB from "./db/index.js";
 
+const app = express();
 
 connectDB()
     .then(() => {
